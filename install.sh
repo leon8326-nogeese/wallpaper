1b4590d0b5deb12e624629180e3e944b26dc7113
Nogeese Linux <leon8326nogeese@outlook.com>
2025-01-25 21:55:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,14 +12,26 @@ clear
 echo "Downloading metadata..."
 curl -sSL https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/metadata.json >> metadata.json 
 clear
-echo "Downloading image (1/4)..."
-https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images/main.png >> contents/images/3840x2160.png 
+echo "Downloading image (1/8)..."
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images/3840x2160.png >> contents/images/3840x2160.png 
 clear
-echo "Downloading image (2/4)..."
-https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images/main-vertical.png >> contents/images/1080x2280.png
+echo "Downloading image (2/8)..."
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images/1080x2280.png >> contents/images/1080x2280.png
 clear
-echo "Downloading image (3/4)..."
-https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images-dark/main.png >> contents/images-dark/3840x2160.png 
+echo "Downloading image (3/8)..."
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images-dark/3840x2160.png >> contents/images-dark/3840x2160.png 
 clear
-echo "Downloading image (4/4)..."
-https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images-dark/main-vertical.png >> contents/images-dark/1080x2280.png
+echo "Downloading image (4/8)..."
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images-dark/1080x2280.png >> contents/images-dark/1080x2280.png
+clear
+echo "Downloading image (4/8)..."
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images-dark/1024x768.png >> contents/images-dark/1024x768.png
+clear
+echo "Downloading image (4/8)..."
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images/1024x768.png >> contents/images/1024x768.png
+clear
+echo "Downloading image (4/8)..."
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images-dark/440x678.png >> contents/images-dark/440x678.png
+clear
+echo "Downloading image (4/8)..."
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images/440x678.png >> contents/images/440x678.png
