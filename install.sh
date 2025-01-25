1cb3e351a9ce1c47df619e439f61695c8a943330
Nogeese Linux <leon8326nogeese@outlook.com>
2025-01-25 21:24:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,13 +13,13 @@ echo "Downloading metadata..."
 curl -sSL https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/metadata.json >> metadata.json 
 clear
 echo "Downloading image (1/4)..."
-https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images/main.png >> contents/images/main.png 
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images/main.png >> contents/images/3840x2160.png 
 clear
 echo "Downloading image (2/4)..."
-https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images/main-vertical.png >> contents/images/main-vertical.png
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images/main-vertical.png >> contents/images/1080x2280.png
 clear
 echo "Downloading image (3/4)..."
-https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images-dark/main.png >> contents/images-dark/main.png 
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images-dark/main.png >> contents/images-dark/3840x2160.png 
 clear
 echo "Downloading image (4/4)..."
-https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images-dark/main-vertical.png >> contents/images-dark/main-vertical.png 
+https://raw.githubusercontent.com/leon8326-nogeese/wallpaper/main/Next/contents/images-dark/main-vertical.png >> contents/images-dark/1080x2280.png
